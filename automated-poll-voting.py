e3eaca592cd2e7935a975d9c5ba4cdd44f9a37d5
Genes22 <genes0022@gmail.com>
2019-04-22 10:26:26 +0300

    The automatic voting bot in easypolls

--- automated-poll-voting.py
+++ automated-poll-voting.py
@@ -2,9 +2,11 @@ from selenium import webdriver
 import time
 
 url = 'http://www.easypolls.net/poll.html?p=5cb826a3e4b01977b1968d91'
+#Opening chrome tab and the url
 driver = webdriver.Chrome()
 driver.get(url)
 
+#vote 10 times on the selected option and delete the cookies after that@@
 for i in range(10):
 	driver.get(url)
 	driver.find_element_by_xpath('//*[@id="c_2_5cb826a3e4b01977b1968d91"]').click()
