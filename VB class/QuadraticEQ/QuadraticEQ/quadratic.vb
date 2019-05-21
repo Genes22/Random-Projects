0ddded855f694f5af7699ba5a463180ea25582ff
Genes22 <genes0022@gmail.com>
2019-05-21 13:09:16 +0300

    moved soundtotext files to Bravos && Quadratic code not calculating after checking for a<>0 in the conditions

--- VB class/QuadraticEQ/QuadraticEQ/quadratic.vb
+++ VB class/QuadraticEQ/QuadraticEQ/quadratic.vb
@@ -11,17 +11,20 @@
 
         Console.WriteLine("Enter c:")
         c = Console.ReadLine()
-
-        num = Math.Sqrt((b ^ 2) - (4 * a * c))
-        If num > 0 Then
-            x1 = ((-b) + num) / (2 * a)
-            x2 = ((-b) - num) / (2 * a)
-            Console.WriteLine("The value of x1 is {0}", x1)
-            Console.WriteLine("The value of x2 is {0}", x2)
-            Console.ReadLine()
-        ElseIf num == 0 Then
-            x = (-b) / (2 * a)
-            Console.WriteLine("The value of X1 and X2 is {0}", x)
-        End If
+        If a = 0 Then
+            console.writeline("The value of A connot be zero")
+        Else
+            num = Math.Sqrt((b ^ 2) - (4 * a * c))
+            If num > 0 Then
+                x1 = ((-b) + num) / (2 * a)
+                x2 = ((-b) - num) / (2 * a)
+                Console.WriteLine("The value of x1 is {0}", x1)
+                Console.WriteLine("The value of x2 is {0}", x2)
+            ElseIf num = 0 Then
+                x = (-b) / (2 * a)
+                Console.WriteLine("The value of X1 and X2 is {0}", x)
+            End If
+        End if
+        console.readline()
     End Sub
 End Module
