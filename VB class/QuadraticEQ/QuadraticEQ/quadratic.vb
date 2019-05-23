10d6e3d0ee7d3bc0a9a2de9dee89ec5f945de3b2
Genes22 <genes0022@gmail.com>
2019-05-23 15:01:20 +0300

    working quadratic and calculator

--- VB class/QuadraticEQ/QuadraticEQ/quadratic.vb
+++ VB class/QuadraticEQ/QuadraticEQ/quadratic.vb
@@ -1,30 +1,33 @@
 ﻿Module quadratic
     Sub Main()
-        Dim a, b, c As Integer
-        Dim x1, x2, x, num As Double
+        Do
+            Dim a, b, c As Integer
+            Dim x1, x2, x, num As Double
 
-        Console.WriteLine("Enter a: ")
-        a = Console.ReadLine()
+            Console.WriteLine("Enter a: ")
+            a = Console.ReadLine()
 
-        Console.WriteLine("Enter b:")
-        b = Console.ReadLine()
+            Console.WriteLine("Enter b:")
+            b = Console.ReadLine()
 
-        Console.WriteLine("Enter c:")
-        c = Console.ReadLine()
-        If a = 0 Then
-            console.writeline("The value of A connot be zero")
-        Else
-            num = Math.Sqrt((b ^ 2) - (4 * a * c))
-            If num > 0 Then
-                x1 = ((-b) + num) / (2 * a)
-                x2 = ((-b) - num) / (2 * a)
-                Console.WriteLine("The value of x1 is {0}", x1)
-                Console.WriteLine("The value of x2 is {0}", x2)
-            ElseIf num = 0 Then
-                x = (-b) / (2 * a)
-                Console.WriteLine("The value of X1 and X2 is {0}", x)
+            Console.WriteLine("Enter c:")
+            c = Console.ReadLine()
+
+            If a = 0 Then
+                Console.WriteLine("The value of A cannot be 0 in Quadratic equations")
+            Else
+                num = Math.Sqrt((b ^ 2) - (4 * a * c))
+                If num > 0 Then
+                    x1 = ((-b) + num) / (2 * a)
+                    x2 = ((-b) - num) / (2 * a)
+                    Console.WriteLine("The value of x1 is {0}", x1)
+                    Console.WriteLine("The value of x2 is {0}", x2)
+                ElseIf num = 0 Then
+                    x = (-b) / (2 * a)
+                    Console.WriteLine("The value of X1 and X2 is {0}", x)
+                End If
             End If
-        End if
-        console.readline()
+            Console.ReadLine()
+        Loop
     End Sub
 End Module
