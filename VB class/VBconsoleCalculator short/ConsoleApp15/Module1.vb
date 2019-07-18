a66add099a08f2e25fb6f51d9bdb8512bcbc8dff
k3nny-pkfr <vectorenx88@gmail.com>
2019-07-18 20:01:19 +0300

    added short calc

---
+++ VB class/VBconsoleCalculator short/ConsoleApp15/Module1.vb
@@ -0,0 +1,81 @@
+﻿Module Module1
+    Dim firstnum, zeroerr As Double
+    Dim secondnum As Double
+    Dim answer As Double
+    Dim mathOperator As String
+    Dim result As String
+
+    Function Addition(ByVal num1 As Double, ByVal num2 As Double) As Double
+        result = num1 + num2
+        Return result
+    End Function
+
+    Function Minus(ByVal num1 As Double, ByVal num2 As Double) As Double
+        result = num1 - num2
+        Return result
+    End Function
+
+    Function Multiplication(ByVal num1 As Double, ByVal num2 As Double) As Double
+        result = num1 * num2
+        Return result
+    End Function
+
+    Function Division(ByVal num1 As Double, ByVal num2 As Double) As Double
+        result = num1 / num2
+        Return result
+    End Function
+
+    Function Remainder(ByVal num1 As Double, ByVal num2 As Double) As Double
+        result = num1 Mod num2
+        Return result
+    End Function
+
+    Sub Main()
+
+        Console.WriteLine("Console Calculator Tutorial" + vbNewLine)
+
+            Console.WriteLine("Enter the first number" + vbNewLine)
+            firstnum = Double.Parse(Console.ReadLine)
+
+            Console.WriteLine("Select any operator:+,-,*,/" + vbNewLine)
+            mathOperator = Console.ReadLine
+
+            Console.WriteLine("Enter the second number" + vbNewLine)
+        secondnum = Double.Parse(Console.ReadLine)
+
+        If secondnum = 0 Then
+            Console.WriteLine("Retry second number")
+            zeroerr = Console.ReadLine()
+            answer = firstnum / zeroerr
+
+        Else
+            answer = firstnum / secondnum
+
+        End If
+
+
+        Select Case mathOperator
+
+                Case ("+")
+                    answer = firstnum + secondnum
+
+                Case ("-")
+                    answer = firstnum - secondnum
+
+                Case ("*")
+                    answer = firstnum * secondnum
+
+                Case ("/")
+
+
+
+        End Select
+            Console.Write(answer)
+            Console.Write(vbNewLine + "press any key to exit.....")
+            Console.ReadKey()
+
+
+
+    End Sub
+
+End Module
---
+++ VB class/VBconsoleCalculator short/vbCalculator.vb
@@ -0,0 +1,62 @@
+﻿Module vbCalculator
+    Dim result As String
+    Function Addition(ByRef num1 As Double, ByRef num2 As Double) As Double
+        result = num1 + num2
+        Return result
+    End Function
+
+    Function Minus(ByRef num1 As Double, ByRef num2 As Double) As Double
+        result = num1 - num2
+        Return result
+    End Function
+
+    Function Multiplication(ByRef num1 As Double, ByRef num2 As Double) As Double
+        result = num1 * num2
+        Return result
+    End Function
+
+    Function Division(ByRef num1 As Double, ByRef num2 As Double) As Double
+        result = num1 / num2
+        Return result
+    End Function
+    Sub Main()
+        Do
+            Dim num1, num2 As Double
+            Dim operation, result As String
+            Console.Write("Enter the first number: ")
+            num1 = Console.ReadLine()
+            Console.WriteLine("Enter the operation you want to perform: " + vbLf + " + or add: " + vbLf + " - or sub: " + vbLf + " * or mult: " + vbLf + "/ or div:")
+            operation = Console.ReadLine()
+            Console.Write("Enter the second number: ")
+            num2 = Console.ReadLine()
+
+            If operation.Contains("+") Or operation.Contains("add") Then
+                result = Addition(num1, num2)
+                Console.WriteLine("The sum of {0} and {1} is: {2}", num1, num2, result)
+            ElseIf operation.Contains("-") Or operation.Contains("sub") Then
+                result = Minus(num1, num2)
+                Console.WriteLine("The solution  of {0} minus {1} is: {2}", num1, num2, result)
+            ElseIf operation.Contains("*") Or operation.Contains("mult") Then
+                result = Multiplication(num1, num2)
+                Console.WriteLine("The product of {0} and {1} is: {2}", num1, num2, result)
+            ElseIf operation.Contains("/") Or operation.Contains("div") Then
+                If num2 = 0 Then
+                    Dim num3, num4 As Double
+                    Console.WriteLine("The divider cannot be zero ")
+                    Console.Write("Enter the first number again: ")
+                    num3 = Console.ReadLine()
+                    Console.Write("Enter a non zero second number: ")
+                    num4 = Console.ReadLine()
+                    result = num3 / num4
+                    Console.WriteLine("The result of {0} divided by {1} is: {2}", num3, num4, result)
+                Else
+                    result = Division(num1, num2)
+                    Console.WriteLine("The solution of {0} divided by {1} is: {2}", num1, num2, result)
+                End If
+            Else
+                Console.WriteLine("Enter valid values")
+            End If
+            Console.ReadLine()
+        Loop
+    End Sub
+End Module
